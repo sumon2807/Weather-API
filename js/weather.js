8a955d133ad32e37d238bfb12e410f8cadded933
sumon2807 <sharifulbd911@gmail.com>
2023-03-07 01:48:59 +0600

    city temperature is dynamically add

--- js/weather.js
+++ js/weather.js
@@ -1,12 +1,27 @@
 
 const API_KEYS=`d8aae2363e9a8de20ffd5ddf257bb277`;
 
-const weatherData=city=>{
-    const url=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEYS}`;
+const loadTemperature=city=>{
+    const url=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEYS}&units=metric`;
     fetch(url)
     .then (res=>res.json())
-    .then (data=>console.log(data))
+    .then (data=>displayTemperature(data))
 }
-weatherData();
+const displayTemperature=data=>{
+    const temperature=document.getElementById('temperature')
+    temperature.innerText=data.main.temp;
+    const weather=document.getElementById('weather')
+    weather.innerText=data.weather[0].main;
+}
+document.getElementById('search-btn').addEventListener('click', function(){
+    const searchField=document.getElementById('search-field');
+    const searchText=searchField.value;
+    searchField.value='';
+    // set city
+    document.getElementById('city-name').innerText=searchText;
+    loadTemperature(searchText);
+})
+
+loadTemperature('dhaka');
 
 
